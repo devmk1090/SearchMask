4ab2f664bec4d0e302c23bcc19c9ee329712bcc9
devmk1090 <devmk1090@gmail.com>
2020-03-27 20:50:00 +0900

    - 내 주변 1km 내의 마스크 정보를 NaverMap 에 모두 표시 완료 - 다음은 UI 변경 및 버그 개선

--- app/src/main/java/com/devkproject/searchmask/MapActivity.kt
+++ app/src/main/java/com/devkproject/searchmask/MapActivity.kt
@@ -4,32 +4,31 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import com.devkproject.searchmask.api.MaskClient
 import com.devkproject.searchmask.api.MaskInterface
-import com.devkproject.searchmask.model.MaskDetailResponse
 import com.devkproject.searchmask.model.MaskModel
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.overlay.OverlayImage
 import kotlinx.android.synthetic.main.activity_map.*
-import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     var latitude: Double? = null
     var longitude: Double? = null
-    val marker = Marker()
+    private val marker = Marker()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-
         latitude = intent.getDoubleExtra("latitude", 0.0)
         longitude = intent.getDoubleExtra("longitude", 0.0)
 
@@ -41,64 +40,64 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(naverMap: NaverMap) {
         val cameraUpdate = CameraUpdate.scrollTo(LatLng(latitude!!, longitude!!)).animate(CameraAnimation.Fly)
         naverMap.moveCamera(cameraUpdate)
-
-//        val infoWindow = InfoWindow()
-//        infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(this) {
-//            override fun getText(p0: InfoWindow): CharSequence {
-//                return "현재 위치"
-//            }
-//        }
-//        infoWindow.open(marker)
+        val infoWindow = InfoWindow()
+        infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(this) {
+            override fun getText(p0: InfoWindow): CharSequence {
+                return infoWindow.marker?.tag as CharSequence? ?: ""
+            }
+        }
         val maskService: MaskInterface = MaskClient.getClient()
         maskService.getMaskGeo(latitude!!,longitude!!,1000).enqueue(object : Callback<MaskModel> {
             override fun onResponse(call: Call<MaskModel>, response: Response<MaskModel>) {
                 if (response != null && response.isSuccessful) {
                     Log.d("MaskActivity", "성공 : " + response.body().toString())
-                    jsonObject(response.body())
+                    val results : MaskModel? = response.body()
+
+                    val markers = mutableListOf<Marker>()
+                    val image = OverlayImage.fromResource(R.drawable.map_icon)
+                    for (i in results!!.stores) {
+                        val name = i?.name
+                        val lat = i?.lat
+                        val lng = i?.lng
+                        val remain = i?.remain_stat
+                        val addr = i?.addr
+                        val stock = i?.stock_at
+                        val create = i?.created_at
+
+                        markers += Marker().apply {
+                            position = LatLng(lat!!, lng!!)
+                            captionText = name!!
+                            when {
+                                remain.equals("break") -> subCaptionText = "판매중지"
+                                remain.equals("empty") -> subCaptionText = "품절"
+                                remain.equals("few") -> subCaptionText = "30개 미만"
+                                remain.equals("some") -> subCaptionText = "99 ~ 30개"
+                                remain.equals("plenty") -> subCaptionText = "100개 이상"
+                            }
+                            subCaptionColor = Color.BLUE
+                            icon = image
+                            tag = "$addr\n입고 시간 : $stock\n마지막 업데이트 : $create"
+                            setOnClickListener {
+                                infoWindow.open(this)
+                                true
+                            }
+                            isHideCollidedCaptions = true
+                            map = naverMap
+                        }
+                    }
                 }
             }
             override fun onFailure(call: Call<MaskModel>, t: Throwable) {
                 Log.d("MaskActivity", "실패")
             }
         })
+        naverMap.setOnMapClickListener { pointF, latLng ->
+            infoWindow.close()
+        }
         marker.position = LatLng(latitude!!, longitude!!)
+        marker.width = 70
+        marker.height = 110
         marker.captionText = "현재 위치"
         marker.map = naverMap
     }
-
-    fun api(latitude: Double, longitude: Double) {
-
-    }
-
-    fun jsonObject (data: MaskModel?) {
-        val maskDetail : MaskDetailResponse? = data?.stores?.get(0)
-        maskDetail?.let {
-
-        }
-        val jObject : JSONObject = JSONObject(data.toString())
-        val jArray = jObject.getJSONArray("stores")
-        for (i in 0 until jArray.length()) {
-            val obj = jArray.getJSONObject(i)
-            val name = obj.getString("name")
-            Log.d("MaskActivity", name)
-        }
-    }
-
 }
-//                    body?.let {
-//                        if (it.stores != null) {
-//                            for(i in 0 until it.stores.size) {
-//                                val name = it.stores[i]?.name
-//                                val addr = i!!.addr
-//                                val lat = it.stores[i]?.lat
-//                                val lng = it.stores[i]?.lng
-//                                val remain = it.stores[i]?.remain_stat
-//                                val stock = i!!.stock_at
-//                                val create = i!!.created_at
-//                                Log.d("MapActivity", name)
-//                                marker.position = LatLng(lat!!, lng!!)
-//                                marker.captionText = name!!
-//                                marker.subCaptionText = "재고 : $remain"
-//                                marker.subCaptionColor = Color.BLUE
-//                            }
-//                            }
